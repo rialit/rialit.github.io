76b39371c77af0e8cd00293139cdb149d5cf893b
rialit <r1al1t@mail.ru>
2021-12-10 11:13:30 +0700

    ll

--- publish/script.js
+++ publish/script.js
@@ -1496,10 +1496,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/index.js");
+
 
 
 var WorkPage = function WorkPage(props) {
-  console.log(props);
+  var urlProps = (0,react_router_dom__WEBPACK_IMPORTED_MODULE_1__.useParams)();
+  console.log(props, urlProps);
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: ""
   }, "Works");
